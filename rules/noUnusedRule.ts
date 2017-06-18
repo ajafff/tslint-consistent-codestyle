c6fce0f4375f91e99bd78af827ae1f87499a4ef3
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-06-18 20:49:28 +0200

    no-unused: initial implementation

---
+++ rules/noUnusedRule.ts
@@ -0,0 +1,130 @@
+import * as ts from 'typescript';
+import * as Lint from 'tslint';
+import {
+    isParameterDeclaration, isParameterProperty, isFunctionWithBody, isFunctionExpression, isExpressionValueUsed,
+    getDeclarationDomain, collectVariableUsage, VariableInfo, VariableUse, DeclarationDomain, UsageDomain,
+} from 'tsutils';
+
+export class Rule extends Lint.Rules.AbstractRule {
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new UnusedWalker(sourceFile, this.ruleName, undefined));
+    }
+}
+
+class UnusedWalker extends Lint.AbstractWalker<void> {
+    public walk(sourceFile: ts.SourceFile) {
+        if (sourceFile.isDeclarationFile)
+            return;
+        const usage = collectVariableUsage(sourceFile);
+        usage.forEach((variableInfo, identifier) => {
+            if (isExcluded(variableInfo, sourceFile))
+                return;
+            let uses = getUsesByDomain(variableInfo.uses, getDeclarationDomain(identifier)!);
+            if (uses.length === 0)
+                return this.addFailureAtNode(identifier, `${showKind(identifier)} '${identifier.text}' is unused.`);
+            uses = filterWriteOnly(uses);
+            if (uses.length === 0)
+                return this.addFailureAtNode(identifier, `${showKind(identifier)} '${identifier.text}' is only written and never read.`);
+            // TODO error for classes / functions only used inside of their body
+            // TODO error for classes / functions only used mutually recursive
+        });
+    }
+}
+
+function getUsesByDomain(uses: VariableUse[], domain: DeclarationDomain): VariableUse[] {
+    const result = [];
+    for (const use of uses)
+        if (use.domain & domain)
+            result.push(use);
+    return result;
+}
+
+function filterWriteOnly(uses: VariableUse[]): VariableUse[] {
+    // TODO handle foo = foo + 1;
+    const result = [];
+    for (const use of uses)
+        if (use.domain & UsageDomain.Type || isExpressionValueUsed(use.location))
+            result.push(use);
+    return result;
+}
+
+function isExcluded(variable: VariableInfo, sourceFile: ts.SourceFile): boolean {
+    if (variable.exported || variable.inGlobalScope)
+        return true;
+    for (const declaration of variable.declarations) {
+        const parent = declaration.parent!;
+        if (declaration.text.startsWith('_')) {
+            switch (parent.kind) {
+                case ts.SyntaxKind.Parameter:
+                    return true;
+                case ts.SyntaxKind.VariableDeclaration:
+                    if (parent.parent!.parent!.kind === ts.SyntaxKind.ForInStatement ||
+                        parent.parent!.parent!.kind === ts.SyntaxKind.ForOfStatement)
+                        return true;
+                    break;
+                case ts.SyntaxKind.BindingElement:
+                    if ((<ts.BindingElement>parent).dotDotDotToken !== undefined)
+                        break;
+                    const pattern = <ts.BindingPattern>parent.parent;
+                    if (pattern.kind === ts.SyntaxKind.ObjectBindingPattern &&
+                        pattern.elements[pattern.elements.length - 1].dotDotDotToken !== undefined)
+                        return true;
+            }
+        }
+        if (isParameterDeclaration(parent) &&
+            (isParameterProperty(parent) || !isFunctionWithBody(parent.parent!)))
+            return true;
+        if (parent.kind === ts.SyntaxKind.VariableDeclaration && parent.parent!.kind === ts.SyntaxKind.CatchClause)
+            return true;
+        // don't show any error on function expressions whose name is unused
+        if (isFunctionExpression(parent) && isExpressionValueUsed(parent))
+            return true;
+        // exclude imports in TypeScript files, because is may be used implicitly by the declaration emitter
+        if (/.tsx?$/.test(sourceFile.fileName)) {
+            switch (parent.kind) {
+                case ts.SyntaxKind.ImportEqualsDeclaration:
+                    if ((<ts.ImportEqualsDeclaration>parent).moduleReference.kind === ts.SyntaxKind.ExternalModuleReference)
+                        return true;
+                    break;
+                case ts.SyntaxKind.NamespaceImport:
+                case ts.SyntaxKind.NamedImports:
+                case ts.SyntaxKind.ImportSpecifier:
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
+function showKind(node: ts.Identifier): string {
+    switch (node.parent!.kind) {
+        case ts.SyntaxKind.BindingElement:
+        case ts.SyntaxKind.VariableDeclaration:
+            return 'Variable';
+        case ts.SyntaxKind.Parameter:
+            return 'Parameter';
+        case ts.SyntaxKind.FunctionDeclaration:
+        case ts.SyntaxKind.FunctionExpression:
+            return 'Function';
+        case ts.SyntaxKind.ClassDeclaration:
+        case ts.SyntaxKind.ClassExpression:
+            return 'Class';
+        case ts.SyntaxKind.InterfaceDeclaration:
+            return 'Interface';
+        case ts.SyntaxKind.NamespaceImport:
+        case ts.SyntaxKind.NamedImports:
+        case ts.SyntaxKind.ImportSpecifier:
+        case ts.SyntaxKind.ImportEqualsDeclaration:
+            return 'Import';
+        case ts.SyntaxKind.EnumDeclaration:
+            return 'Enum';
+        case ts.SyntaxKind.ModuleDeclaration:
+            return 'Namespace';
+        case ts.SyntaxKind.TypeAliasDeclaration:
+            return 'TypeAlias';
+        case ts.SyntaxKind.TypeParameter:
+            return 'TypeParameter';
+        default:
+            throw new Error(`Unhandled kind ${node.parent!.kind}: ${ts.SyntaxKind[node.parent!.kind]}`);
+    }
+}

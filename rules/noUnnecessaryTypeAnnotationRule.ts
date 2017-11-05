68f8bd5e123995a1063067e67c3f003390d85261
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-11-05 23:34:43 +0100

    no-unnecessary-type-annotation: check object literal methods and return types

--- rules/noUnnecessaryTypeAnnotationRule.ts
+++ rules/noUnnecessaryTypeAnnotationRule.ts
@@ -1,6 +1,14 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
-import { isTypeParameter, getVariableDeclarationKind, VariableDeclarationKind } from 'tsutils';
+import {
+    isTypeParameter,
+    getVariableDeclarationKind,
+    VariableDeclarationKind,
+    getPropertyName,
+    isTypeFlagSet,
+    isExpressionValueUsed,
+    isUnionType,
+} from 'tsutils';
 
 type FunctionExpressionLike = ts.ArrowFunction | ts.FunctionExpression;
 
@@ -17,9 +25,12 @@ function walk(ctx: Lint.WalkContext<void>, checker: ts.TypeChecker) {
         switch (node.kind) {
             case ts.SyntaxKind.ArrowFunction:
             case ts.SyntaxKind.FunctionExpression:
-            // TODO methods in object literals
-                checkFunctionParameters(<FunctionExpressionLike>node);
+                checkFunction(<FunctionExpressionLike>node);
                 // TODO check return type - add option
+                break;
+            case ts.SyntaxKind.MethodDeclaration:
+                if (node.parent!.kind === ts.SyntaxKind.ObjectLiteralExpression)
+                    checkObjectLiteralMethod(<ts.MethodDeclaration>node);
                 break;
             case ts.SyntaxKind.VariableDeclarationList:
                 // TODO add an option where to put the type annotation for functions (variable or function)
@@ -28,30 +39,46 @@ function walk(ctx: Lint.WalkContext<void>, checker: ts.TypeChecker) {
         return ts.forEachChild(node, cb);
     });
 
-    function checkFunctionParameters(node: FunctionExpressionLike) {
+    function checkFunction(node: FunctionExpressionLike) {
         // TODO use getContextuallyTypedParameterType
-        if (!node.parameters.some((p) => p.type !== undefined && p.dotDotDotToken === undefined))
-            return;
-        if (isIife(node))
-            return checkIife(node.parameters, node.parent.parent.arguments);
+        const iife = getIife(node);
+        if (iife !== undefined)
+            return checkIife(node, iife);
 
-        const contextualType = checker.getContextualType(node);
-        if (contextualType === undefined)
+        const type = getContextualTypeOfFunction(node);
+        if (type === undefined)
             return;
+        checkContextSensitiveFunctionOrMethod(node, type);
+    }
+
+    function checkObjectLiteralMethod(node: ts.MethodDeclaration) {
+        const type = getContextualTypeOfObjectLiteralMethod(node);
+        if (type === undefined)
+            return;
+        checkContextSensitiveFunctionOrMethod(node, type);
+    }
+
+    function checkContextSensitiveFunctionOrMethod(node: ts.FunctionLikeDeclaration, contextualType: ts.Type) {
         const callSignatures = contextualType.getCallSignatures();
         // TODO choose the correct signature?
         if (callSignatures.length !== 1)
             return;
         const signature = callSignatures[0];
+        if (node.type !== undefined && !signatureHasGenericReturn(signature) &&
+            typesAreEqual(checker.getTypeFromTypeNode(node.type), signature.getReturnType()))
+            fail(node.type);
+
+        if (!node.parameters.some((p) => p.type !== undefined && p.dotDotDotToken === undefined))
+            return;
+
         for (let i = 0; i < node.parameters.length; ++i) {
             const parameter = node.parameters[i];
             if (parameter.dotDotDotToken !== undefined || parameter.type === undefined)
                 continue;
             const context = signature.parameters[i];
-            if (context === undefined || context.declarations === undefined)
+            if (context === undefined)
                 break;
-            const declaredType = checker.getTypeAtLocation(context.declarations[0]);
-            if (isTypeParameter(declaredType))
+            if (context.declarations !== undefined && isTypeParameter(checker.getTypeAtLocation(context.declarations[0])))
                 continue;
             const type = checker.getTypeFromTypeNode(parameter.type);
             const contextualParameterType = checker.getTypeOfSymbolAtLocation(context, node);
@@ -60,7 +87,19 @@ function walk(ctx: Lint.WalkContext<void>, checker: ts.TypeChecker) {
         }
     }
 
-    function checkIife(parameters: ReadonlyArray<ts.ParameterDeclaration>, args: ReadonlyArray<ts.Expression>) {
+    function checkIife(func: FunctionExpressionLike, iife: ts.CallExpression) {
+        if (func.type !== undefined && func.name === undefined &&
+            (
+                !isExpressionValueUsed(iife) ||
+                !containsLiteralType(checker.getTypeFromTypeNode(func.type)) && checker.getContextualType(iife) !== undefined
+            ))
+            fail(func.type);
+
+        const {parameters} = func;
+        if (!parameters.some((p) => p.type !== undefined && p.dotDotDotToken === undefined))
+            return;
+
+        const args = iife.arguments;
         const len = Math.min(parameters.length, args.length);
         for (let i = 0; i < len; ++i) {
             const {type, dotDotDotToken} = parameters[i];
@@ -93,10 +132,45 @@ function walk(ctx: Lint.WalkContext<void>, checker: ts.TypeChecker) {
     function typesAreEqual(a: ts.Type, b: ts.Type): boolean {
         return a === b || checker.typeToString(a) === checker.typeToString(b);
     }
+
+    function getContextualTypeOfFunction(func: FunctionExpressionLike): ts.Type | undefined {
+        const type = checker.getContextualType(func);
+        return type && checker.getApparentType(type);
+    }
+
+    function getContextualTypeOfObjectLiteralMethod(method: ts.MethodDeclaration): ts.Type | undefined {
+        const name = getPropertyName(method.name);
+        if (name === undefined)
+            return;
+        let type = checker.getTypeAtLocation(method.parent!);
+        if (type === undefined)
+            return;
+        type = checker.getApparentType(type);
+        if (!isTypeFlagSet(type, ts.TypeFlags.StructuredType))
+            return;
+        const symbol = type.getProperty(name);
+        return symbol && checker.getTypeOfSymbolAtLocation(symbol, method.name);
+    }
+
+    function signatureHasGenericReturn(signature: ts.Signature): boolean {
+        const original = checker.getSignatureFromDeclaration(signature.declaration);
+        return original !== undefined && isTypeParameter(original.getReturnType());
+    }
 }
 
-function isIife<T extends FunctionExpressionLike>(node: T): node is T & {parent: ts.ParenthesizedExpression & {parent: ts.CallExpression}} {
-    return node.parent!.kind === ts.SyntaxKind.ParenthesizedExpression &&
-        node.parent!.parent!.kind === ts.SyntaxKind.CallExpression &&
-        (<ts.CallExpression>node.parent!.parent).expression === node.parent;
+function getIife(node: FunctionExpressionLike): ts.CallExpression | undefined {
+    if (node.parent!.kind !== ts.SyntaxKind.ParenthesizedExpression)
+        return;
+    let prev = node.parent!;
+    let parent = prev.parent!;
+    while (parent.kind === ts.SyntaxKind.ParenthesizedExpression) {
+        prev = parent;
+        parent = parent.parent!;
+    }
+    if (parent.kind === ts.SyntaxKind.CallExpression && (<ts.CallExpression>parent).expression === prev)
+        return <ts.CallExpression>parent;
+}
+
+function containsLiteralType(type: ts.Type): boolean {
+    return isUnionType(type) ? type.types.some(containsLiteralType) : isTypeFlagSet(type, ts.TypeFlags.Literal);
 }
